bb5bcb034e2969b38df6c3e84e5123d8c5bd8142
joseph.gitau <joseph.gitau@canter.fi>
2020-03-13 15:43:41 +0200

    add create movie form template

--- src/app/movie/movie-routing.module.ts
+++ src/app/movie/movie-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { CreateMovieComponent } from './create-movie/create-movie.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MovieListComponent
+  },
+  {
+    path: 'create',
+    component: CreateMovieComponent
   },
   {
     path: ':id',
--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -5,9 +5,10 @@ import { MovieRoutingModule } from './movie-routing.module';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MovieComponent } from './movie/movie.component';
+import { CreateMovieComponent } from './create-movie/create-movie.component';
 
 @NgModule({
-  declarations: [MovieListComponent, MovieDetailComponent, MovieComponent],
+  declarations: [MovieListComponent, MovieDetailComponent, MovieComponent, CreateMovieComponent],
   imports: [
     CommonModule,
     MovieRoutingModule
